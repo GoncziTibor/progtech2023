881413ad13ab40f89c33fb51176c1e23fe228c4b
GoncziTibor <79456470+GoncziTibor@users.noreply.github.com>
2023-10-26 23:01:42 +0200

    main felépítése - switch elkezdése

--- progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/Main.java
+++ progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/Main.java
@@ -8,5 +8,26 @@ public class Main {
 
         String userName = User.getUsername();
         User.greetUser(userName);
+
+        boolean game = false;
+
+        while (!game){
+            int answer =;
+
+            switch (answer) {
+                case 1:
+
+                case 2:
+
+                case 3:
+
+                case 4:
+
+                case 5:
+
+                case 6:
+            }
+
+        }
     }
 }
