15c245d7357e40c475a04edac47f77264b86abb4
GoncziTibor <79456470+GoncziTibor@users.noreply.github.com>
2023-10-28 20:35:18 +0200

    playerinterface

---
+++ progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/model/PlayerInterface.java
@@ -0,0 +1,13 @@
+package wumpus.model;
+
+public class PlayerInterface {
+    public void move();
+
+    public void turnRight();
+
+    public void turnLeft();
+
+    public void shoot();
+
+    public void gold();
+}
