c27301119bd8514ea8d7954e53f0ebb3fbb517ba
GoncziTibor <79456470+GoncziTibor@users.noreply.github.com>
2023-10-28 20:28:08 +0200

    player+konstuktor

--- progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/model/Player.java
+++ progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/model/Player.java
@@ -1,4 +1,20 @@
 package wumpus.model;
 
 public class Player {
+    private char column;
+    private int row;
+    private char direction;
+    private int arrows;
+    private int gold;
+
+    public Player(char column, int row, char direction, int arrows, int gold) {
+        this.column = column;
+        this.row = row;
+        this.direction = direction;
+        this.arrows = arrows;
+        this.gold = gold;
+    }
+
+    public Player() {
+    }
 }
