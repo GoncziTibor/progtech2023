866699c0ecb728f96ba4bd5e1498f0358f9d9b39
GoncziTibor <79456470+GoncziTibor@users.noreply.github.com>
2023-10-28 20:38:59 +0200

    player+többi

--- progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/model/Player.java
+++ progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/model/Player.java
@@ -1,6 +1,6 @@
 package wumpus.model;
 
-public class Player {
+public class Player{
     private char column;
     private int row;
     private char direction;
@@ -17,4 +17,82 @@ public class Player {
 
     public Player() {
     }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public void setColumn(char column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
+    }
+
+    public int getArrows() {
+        return arrows;
+    }
+
+    public void setArrows(int arrows) {
+        this.arrows = arrows;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        if (column != player.column) return false;
+        if (row != player.row) return false;
+        if (direction != player.direction) return false;
+        if (arrows != player.arrows) return false;
+        return gold == player.gold;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = column;
+        result = 31 * result + row;
+        result = 31 * result + (int) direction;
+        result = 31 * result + arrows;
+        result = 31 * result + gold;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Player{");
+        sb.append("column=").append(column);
+        sb.append(", row=").append(row);
+        sb.append(", direction=").append(direction);
+        sb.append(", arrows=").append(arrows);
+        sb.append(", gold=").append(gold);
+        sb.append('}');
+        return sb.toString();
+    }
+
+
 }
