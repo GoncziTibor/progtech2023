faabeca5e568597614fc6bb7da7bb70748cccd13
GoncziTibor <79456470+GoncziTibor@users.noreply.github.com>
2023-10-28 20:26:36 +0200

    map+konstruktor

--- progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/model/Map.java
+++ progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/model/Map.java
@@ -1,4 +1,13 @@
 package wumpus.model;
 
 public class Map {
+    private int size;
+    private char[][] map;
+    private Player player;
+
+    public Map(int size, char[][] map, Player player) {
+        this.size = size;
+        this.map = map;
+        this.player = player;
+    }
 }
