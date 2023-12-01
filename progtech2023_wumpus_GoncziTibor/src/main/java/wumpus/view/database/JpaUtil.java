785c61b09a023725714eefb553a2b37971dc6575
GoncziTibor <79456470+GoncziTibor@users.noreply.github.com>
2023-12-01 15:20:06 +0100

    xml6

---
+++ progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/view/database/JpaUtil.java
@@ -0,0 +1,28 @@
+package wumpus.view.database;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+    private static final String PERSISTENCE_UNIT_NAME = "my-persistence-unit";
+    private static EntityManagerFactory entityManagerFactory;
+
+    static {
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
+
+    public static void closeEntityManagerFactory() {
+        if (entityManagerFactory != null && entityManagerFactory.isOpen()) {
+            entityManagerFactory.close();
+        }
+    }
+}
