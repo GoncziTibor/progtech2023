79ed5f579b443845b4baa443e21c67a9c7043665
GoncziTibor <79456470+GoncziTibor@users.noreply.github.com>
2023-12-01 11:59:28 +0100

    xml 1

---
+++ progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/view/database/GameData.java
@@ -0,0 +1,4 @@
+package wumpus.view.database;
+
+public class GameData {
+}
---
+++ progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/view/database/XmlDataSaver.java
@@ -0,0 +1,38 @@
+package wumpus.view.database;
+
+import wumpus.view.database.GameData;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+
+
+public class XmlDataSaver {
+
+    public static void saveGameData(GameData gameData, String filePath) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(GameData.class);
+            Marshaller marshaller = context.createMarshaller();
+
+            // Formázott kimenet
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+            // Menti az adatokat XML-be
+            marshaller.marshal(gameData, new File(filePath));
+
+            System.out.println("Adatok sikeresen elmentve az XML-be.");
+        } catch (JAXBException e) {
+            e.printStackTrace();
+            System.out.println("Hiba az adatok mentése közben.");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Példa adatok létrehozása és mentése XML-be
+        GameData gameData = new GameData();
+        // Állítsd be a játék adatait...
+
+        saveGameData(gameData, "gameData.xml");
+    }
+}
