f26f04af8dd344ee62fbaf36c4d067e9c9047542
GoncziTibor <79456470+GoncziTibor@users.noreply.github.com>
2023-12-02 11:08:10 +0100

    MapHelp test

--- progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/view/map/MapHelp.java
+++ progtech2023_wumpus_GoncziTibor/src/main/java/wumpus/view/map/MapHelp.java
@@ -1,10 +1,9 @@
 package wumpus.view.map;
 
-import wumpus.Main;
-import wumpus.model.Map;
-import wumpus.view.exception.MapReaderException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import wumpus.model.Map;
+import wumpus.view.exception.MapReaderException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,7 +11,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class MapHelp {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static Logger logger = LoggerFactory.getLogger(MapHelp.class);
+
 
     public Map readMap(InputStream inputStream) {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
--- progtech2023_wumpus_GoncziTibor/src/test/java/wumpus/view/map/MapHelpTest.java
+++ progtech2023_wumpus_GoncziTibor/src/test/java/wumpus/view/map/MapHelpTest.java
@@ -1,7 +1,58 @@
 package wumpus.view.map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import wumpus.model.Map;
+import wumpus.view.exception.MapReaderException;
 
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class MapHelpTest {
 
+    @InjectMocks
+    private MapHelp underTest;
+
+    @Mock
+    private Logger loggerMock;
+
+    @Mock
+    private MapReader mapReaderMock;
+
+    @Mock
+    private InputStream inputStreamMock;
+
+    @Test
+    void testReadMap() throws MapReaderException {
+        // GIVEN
+        Map expectedMap = new Map();
+        when(mapReaderMock.readMap()).thenReturn(expectedMap);
+
+        // WHEN
+        Map result = underTest.readMap(inputStreamMock);
+
+        // THEN
+        assertNotNull(result);
+        assertSame(expectedMap, result);
+    }
+
+    @Test
+    void testReadMapWhenMapReaderThrowsException() throws MapReaderException {
+        // GIVEN
+        when(mapReaderMock.readMap()).thenThrow(new MapReaderException("Hiba a térkép olvasása közben"));
+
+        // WHEN
+        Map result = underTest.readMap(inputStreamMock);
+
+        // THEN
+        assertNull(result);
+        verify(loggerMock).error("A pálya beolvasás közben hiba történt!");
+    }
 }
